642bab3d614e3423646223074f4690be9811859d
OP-88 <175477596+OP-88@users.noreply.github.com>
2026-03-30 16:37:55 +0300

    feat(ui): integrate custom application logo

--- ui/electron/main.js
+++ ui/electron/main.js
@@ -9,6 +9,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, '../public/codeatlas_icon.png'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
